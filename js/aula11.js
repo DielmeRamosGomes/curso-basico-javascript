c1341f20ee6eea5eb8d7423a058996e8ef7cfbee
DielmeRamosGomes <dielme.ramos@unesp.br>
2021-11-01 19:32:04 -0300

    atualizando curso

---
+++ js/aula11.js
@@ -0,0 +1,15 @@
+/*
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+*/
+
+var pais = prompt('Digite um pais: ')
+if(pais == 'Brasil') {
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro!')
+}
---
+++ js/main.js
@@ -0,0 +1,74 @@
+//var nome = "Dielme Ramos Gomes";
+//var idade = 27;
+//idade2 = 40
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome + " " + "tem " + idade + " anos");
+// isso é um comentario de uma linha
+/*
+isso é um comentario grande com 
+varias linhas 
+*/
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade+idade2);
+
+//var carros = ["Fusca", "Gol", "Saveiro"];
+//console.log("Tamanho do array = " + carros.length);
+
+/*
+var i;
+for(i = 0 ; i < carros.length ; i++){
+    console.log(carros[i]);
+}
+carros.push("Palio");
+*/
+// alert("Isso é um array? " + Array.isArray(carros));
+/*
+var idade = prompt("Digite uma idade: ");
+if(idade < 18) {
+    alert("Menor de idade");
+}
+else {
+    alert("Maior de idade")
+}
+*/
+/*
+function soma(num1, num2) {
+    return num1+num2;
+}
+*/
+//var numero1 = parseInt(prompt("Digite um numero: "));
+//var numero2 = parseInt(prompt("Digite um numero: "));
+//alert("A Soma do numero "+ numero1 + " mais " + numero2 + " é = " + soma(numero1, numero2));
+/*
+function validaIdade(idade) {
+    var validar;
+    if(idade >= 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+*/
+//var idade = prompt("Qual sua idade? ")
+//console.log(validaIdade(idade));
+/*
+function botao() {
+    alert("Botao Funcionado!");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
