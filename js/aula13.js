579a2f346e67051baed95d0444633cac8ac93e09
DielmeRamosGomes <dielme.ramos@unesp.br>
2021-11-02 16:43:58 -0300

    atualizando o curso de javascript

---
+++ js/aula13.js
@@ -0,0 +1,58 @@
+/*
+var cont  = 0
+while(cont < 6) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+*/
+/*
+var cont = 0
+do{
+    console.log(`Passo ${cont}`)
+    cont++
+}while(cont < 6)
+*/
+
+/*
+for (var i = 0 ; i < 6 ; i++) {
+    console.log(`Passo ${i}`)
+}
+*/
+/*
+let num = [5, 8, 2, 9, 3]
+for(let i = 0 ; i < num.length ; i++) {
+    console.log(`A posição ${i} tem o valor ${num[i]}`)
+}
+*/
+
+/*
+let num = [5, 8, 2, 9, 3]
+for(let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+*/
+
+let num = [5, 8, 2, 9, 3]
+let pos = num.indexOf(2)
+console.log(pos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
