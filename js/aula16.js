2579dbc37758e8a0da674585e600673b9b6622ea
DielmeRamosGomes <dielme.ramos@unesp.br>
2021-11-02 17:13:39 -0300

    curso completo

---
+++ js/aula16.js
@@ -0,0 +1,64 @@
+/*
+function parImpar(n) {
+    if(n % 2 == 0) {
+        return `Par!`
+    } else {
+        return `Impar!`
+    }
+}
+console.log(parImpar(223))
+*/
+/*
+function soma(n1 = 0, n2 = 0) {
+    return n1+n2
+}
+console.log(soma(7, 3))
+*/
+/*
+let v = function(x) {
+    return 2*x
+}
+console.log(v(5))
+*/
+/*
+function fatorial(n) {
+    if(n == 1) {
+        return 1
+    }
+    return n * fatorial(n-1)
+}
+console.log(`O Fatorial é = ${fatorial(5)}`)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
